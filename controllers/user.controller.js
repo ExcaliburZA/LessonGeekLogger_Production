5bd0884632b3c8bd5b0b0f998bd6666fe85c17fb
Samuel Dudley <56539187+ExcaliburZA@users.noreply.github.com>
2023-06-30 13:01:07 +0200

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,7 +13,7 @@ exports.LogIn = async function(req, res) {
     let secret = secrets.SECRET_KEY;
     let password = cryptojs.AES.decrypt(req.headers['password'], secret);
     password = password.toString(cryptojs.enc.Utf8);
-    console.log("LOGIN PASSWORD DECRYPTED: "+password);
+    console.log(password);
     
     Users.find({name: {$eq: req.headers['name']} , password: {$eq: password}})
     .then((ret) => {
