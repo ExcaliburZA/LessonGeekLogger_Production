6bc10b601455d96e1e177386f784651d54105f6e
Samuel Dudley <56539187+ExcaliburZA@users.noreply.github.com>
2023-06-30 12:23:16 +0200

    Add files via upload

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,18 +3,19 @@ const jwt = require('jsonwebtoken');
 const e = require('express');
 const cryptojs = require('crypto-js');
 const secrets = require('../secrets');
+//const Users = require('../frontend/src/models/user_model');
+
 
 
 exports.LogIn = async function(req, res) {
     //console.log("Controller LOGIN called");
 
     let secret = secrets.SECRET_KEY;
-    //let password = cryptojs.AES.decrypt(req.headers['password'], secret);
-    //password = password.toString(cryptojs.enc.Utf8);
+    let password = cryptojs.AES.decrypt(req.headers['password'], secret);
+    password = password.toString(cryptojs.enc.Utf8);
     //console.log(password);
-    //let password = req.headers['password'];
     
-    Users.find({name: {$eq: req.headers['name']} , password: {$eq: req.headers['password']}})
+    Users.find({name: {$eq: req.headers['name']} , password: {$eq: password}})
     .then((ret) => {
         //ret.length > 0 ? console.log(true) : console.log(false); shows correctly
         //console.log(ret[0]);
@@ -38,13 +39,19 @@ exports.LogIn = async function(req, res) {
 }
 
 exports.RegisterUser = async function(req, res) {
+    let decryptedPassword = cryptojs.AES.decrypt(req.headers['password'], secrets.SECRET_KEY);
+    decryptedPassword = decryptedPassword.toString(cryptojs.enc.Utf8);
+
+    let decryptedAccNum = cryptojs.AES.decrypt(req.headers['account_no'], secrets.SECRET_KEY);
+    decryptedAccNum = decryptedAccNum.toString(cryptojs.enc.Utf8); 
+
     let newUser = {
         name: req.headers['name'], 
-        password: req.headers['password'],
+        password: decryptedPassword,
         ID: req.headers['id'],
         phone_no: req.headers['phone_no'],
         email: req.headers['email'],
-        account_no: req.headers['account_no'],
+        account_no: decryptedAccNum,
         university: req.headers['university'],
         course_name: req.headers['course_name'],
         date_of_birth: req.headers['date_of_birth'],
@@ -100,18 +107,18 @@ exports.ApproveUser = async function(req, res){
     })
 }
 
+exports.GetAll = async function(req, res){
+    Users.find()
+    .then((result) => {
+        res.send({result});
+        console.log("Fetched: \n"+result);
+    })
+}
+
 exports.DeleteUser = async function(req, res){
     Users.deleteOne({_id: req.params._id})
     .then((ret) => {
         res.status(200).send({deleted: ret});
-    })
-}
-
-exports.GetAll = async function(req, res){
-    Users.find()
-    .then(result => {
-        res.send({result});
-        console.log("Fetched: \n"+result);
     })
 }
 
